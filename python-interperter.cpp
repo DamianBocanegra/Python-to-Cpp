e56ac8a6e666708fe0c4ec47abf1c1e8fff9431f
Damian <d_b172@txstate.edu>
2019-09-26 18:35:21 -0500

    recognize print command
    program will recognize and execute basic print command

--- python-interperter.cpp
+++ python-interperter.cpp
@@ -1,11 +1,67 @@
 #include <iostream>
+#include<fstream>
 
 using namespace std;
 
+
+void findToken(string);
+string executePrintToken(int, string);
+
+const int MAXLINE = 256;
+
+
+
 int main()
 {
-    cout << "Hello World" << endl;
 
-    return 0;
+  ifstream inFile("python.py");
+  char oneline[MAXLINE];
 
+
+  while(inFile)
+  {
+      inFile.getline(oneline, MAXLINE);
+      findToken(oneline);
+  }
+
+
+  inFile.close();
+
+  return 0;
+}
+
+void findToken(string line)
+{
+    string tokken = "";
+    for(int i = 0; i < MAXLINE; i++)
+    {
+        tokken += line[i];
+
+        if (tokken == "print")
+        {
+            tokken == "";
+            cout << executePrintToken(i, line) << endl;
+            return;
+        }
+    }
+}
+
+string executePrintToken(int position, string line)
+{
+    string printLine = "";
+    for(int k = position; k < MAXLINE; k++)
+    {
+        if(line[k] == '"')
+        {
+            for(int j = k + 1; j < MAXLINE; j++)
+            {
+                if(line[j] == '"')
+                {
+                    return printLine;
+                }
+
+                printLine += line[j];
+            }
+        }
+    }
 }
