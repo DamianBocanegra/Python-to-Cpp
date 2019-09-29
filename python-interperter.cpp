36dc5b684c70f7eb6c1842e4203d1a2d85fb48e6
Damian <d_b172@txstate.edu>
2019-09-29 17:56:20 -0500

    print command
    print command from python can be read and interperted without variables

--- python-interperter.cpp
+++ python-interperter.cpp
@@ -6,24 +6,31 @@ using namespace std;
 
 void findToken(string);
 string executePrintToken(int, string);
+string findWord(string, int);
+bool findFuncStart(string, int);
 
-const int MAXLINE = 256;
-
-
+bool error = false;
 
 int main()
 {
 
   ifstream inFile("python.py");
-  char oneline[MAXLINE];
+  string oneline = "";
 
 
   while(inFile)
   {
-      inFile.getline(oneline, MAXLINE);
+      getline(inFile, oneline);
+      //cout << oneline << endl;
       findToken(oneline);
-  }
 
+      if(error)
+      {
+          cout << "COMPILE ERROR! TERMINATED!" << endl;
+          return 1;
+      }
+      cout << endl;
+  }
 
   inFile.close();
 
@@ -32,36 +39,123 @@ int main()
 
 void findToken(string line)
 {
+    //cout << "ENTER TOKKEN" << endl;
     string tokken = "";
-    for(int i = 0; i < MAXLINE; i++)
+    string printME = "test";
+    bool start = false;
+    for(int i = 0; i < line.length(); i++)
     {
+
         tokken += line[i];
+
 
         if (tokken == "print")
         {
-            tokken == "";
-            cout << executePrintToken(i, line) << endl;
-            return;
+            start = findFuncStart(line, i + 1);
+            if(!start)
+            {
+                error = true;
+                cout << "ERROR '(' MISSING!" << endl;
+
+            }
+            else
+            {
+                printME = executePrintToken(i + 1, line);
+                //cout << "Execute comp" << endl;
+                cout << printME << endl;
+                //cout << "Tokken exit" << endl;
+                return;
+            }
+
         }
     }
+
+
 }
 
 string executePrintToken(int position, string line)
 {
-    string printLine = "";
-    for(int k = position; k < MAXLINE; k++)
+    if(line[line.length() - 1] != ')')
     {
+        error = true;
+        return "ERROR ')' MISSING!";
+    }
+
+    string printLine = "";
+    string result;
+
+    for(int k = position + 1; k < line.length(); k++)
+    {
+        //cout << line[k] << endl;
         if(line[k] == '"')
         {
-            for(int j = k + 1; j < MAXLINE; j++)
+            result = findWord(line, k + 1);
+            if(result != "ERROR!")
             {
-                if(line[j] == '"')
-                {
-                    return printLine;
-                }
-
-                printLine += line[j];
+                k = k + result.length() + 1;
+                printLine += result;
             }
+            else
+            {
+                error = true;
+                return "ERROR OQ";
+            }
+        }
+        else if(line[k] == '+')
+        {
+           //cout << "Found +" << endl;
+        }
+        else if(line[k] == ' ')
+        {
+            //cout << "Found ' '" << endl;
+        }
+        else if(line[k] == '(')
+        {
+            //ignore
+        }
+        else if(line[k] == ')')
+        {
+            return printLine;
+        }
+        else
+        {
+            error = true;
+            return "ERROR INVALID OP";
+        }
+    }
+}
+
+string findWord(string line, int position)
+{
+    string phrase = "";
+    for(int a = position; a < line.length(); a++)
+    {
+        if(line[a] == '"')
+        {
+            return phrase;
+        }
+        phrase += line[a];
+    }
+
+    error = true;
+    return "ERROR!";
+}
+
+bool findFuncStart(string line, int position)
+{
+    for(int b = position; b < line.length(); b++)
+    {
+        if(line[b] == '(')
+        {
+            return true;
+        }
+        else if (line[b] == ' ')
+        {
+            //Ignore
+        }
+        else
+        {
+            return false;
         }
     }
 }
--- python.py
+++ python.py
@@ -1,1 +1,9 @@
 print("Hello World!")
+print("Hello" + "World!")
+print("Hello")
+print  ("Hello")
+print "HELLO"
+print(hello)
+
+
+
